a7a47d7a9e87a2f94adb29802856cbce42484549
pisoc912 <shenyf1124@outlook.com>
2023-12-09 16:08:33 -0800

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from "react";
+import Button from "../components/Button";
+
+interface pageProps {}
+
+const page: FunctionComponent<pageProps> = () => {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+};
+
+export default page;
